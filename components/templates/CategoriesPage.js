4c4e00ddf666aa6c124046d8bd1262bb0f3677eb
arensin8 <arensinani4@gmail.com>
2025-03-15 10:37:51 -0700

    Categories page addings

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import styles from "./CategoriesPage.module.css";
+import { useRouter } from "next/router";
 
 const CategoriesPage = () => {
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+  const router = useRouter();
+
+  const changeHandler = (e) => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  };
+
+  const searchHandler = () => {
+    router.push({
+      pathname: "/categories",
+      query: query,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <h2>Categories</h2>
       <div className={styles.sunContainer}>
         <div className={styles.select}>
-          <select>
+          <select
+            value={query.difficulty}
+            name="difficulty"
+            onChange={changeHandler}
+          >
             <option value="">Difficulty</option>
-            <option value="">Easy</option>
-            <option value="">Medium</option>
-            <option value="">Hard</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
-          <select>
+          <select value={query.time} name="time" onChange={changeHandler}>
             <option value="">Cooking time</option>
-            <option value="">More than 30 min</option>
-            <option value="">Less than 30 min</option>
+            <option value="more">More than 30 min</option>
+            <option value="less">Less than 30 min</option>
           </select>
-          <button>Search</button>
+          <button onClick={searchHandler}>Search</button>
         </div>
       </div>
     </div>
